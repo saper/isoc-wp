55c7c5ea3e639ac272d29f59d08c1d896a41db3f
Dan York <dyork@lodestar2.com>
2012-04-26 22:46:24 -0400

    Fixes issue #23 - meta.php now links to author posts

--- meta.php
+++ meta.php
@@ -13,9 +13,9 @@
 
 	Posted in <?php the_category(', ') ?> | 
 
-	<em>Posted on:</em> <?php the_time('F jS, Y') ?>
+	Posted on: <?php the_time('F jS, Y') ?>
 
-	<em>by</em> <?php the_author() ?> | 
+	by <?php the_author_posts_link() ?> | 
 
 	<?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?>
 
