f874f4d0e374ef9f11f2a2a054fd2aa16cb59cca
Dan York <dyork@lodestar2.com>
2013-10-21 10:30:21 -0400

    Corrects error in meta.php - #74

--- meta.php
+++ meta.php
@@ -9,9 +9,9 @@
 
 <div class="meta">
 
-	<?php <?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?> |
+	<?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?> |
 	Posted in <?php the_category(', ') ?> | 
-    the_tags('Tags: ', ', ', ' | '); ?>
+    <?php the_tags('Tags: ', ', ', ' | '); ?>
 	<?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?>
 
 </div>
