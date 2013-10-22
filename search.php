341632e8f0e67d823d766a18367f6b0b9021b530
Dan York <dyork@lodestar2.com>
2013-10-22 11:58:24 -0400

    Potential fixes to search.php for #88 and #89

--- search.php
+++ search.php
@@ -9,24 +9,51 @@
 
 get_header(); ?>
 
-	<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
+<div id="container">
+<div id="content" role="main">
 
-        <h2><span class=""><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></span></h2>
+<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'isoc-wp' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 
-        <article id="post-<?php the_ID(); ?>" class="post post-page clearfix">
+<!-- Start the Loop. -->
+ <?php if ( have_posts() ) : ?>
+ <ul>
+ <?php while ( have_posts() ) : the_post(); ?>
+ 
+  <div class="post">
+  
+ <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+  
+ <div class="entry">
 
-            <div class="content clearfix">
+   <?php if ( function_exists("has_post_thumbnail") && has_post_thumbnail() ) { the_post_thumbnail(array(200,160), array("class" => "alignright post_thumbnail")); } ?>
+   <?php the_excerpt(); ?><br style="clear:both;"/>
+   <?php get_template_part( 'meta' ); ?>
 
-   				<?php get_template_part( 'meta' ); ?>
+ </div>
+ 
+ </div> <!-- closes the first div box -->
 
-				<?php the_excerpt(''); ?>
+ <!-- Stop The Loop (but note the "else:" - see next line). -->
+ <?php endwhile; ?></ul><?php else: ?>
 
-			</div>
-            
-  		</article>
+ <!-- The very first "if" tested to see if there were any Posts to -->
+ <!-- display.  This "else" part tells what do if there weren't any. -->
+ <p>Sorry, no posts matched your criteria.</p>
 
-	<?php endwhile; ?>
-    <?php endif; ?>
+ <!-- REALLY stop The Loop. -->
+ <?php endif; ?> 
+ 
+<?php get_search_form( $echo ); ?>		
+
+ <!-- Navigation for pagination -->
+ 
+     <div class="navigation">
+       <div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+       <div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+     </div> 
+
+ </div><!-- #content -->
+
+</div><!-- #container -->
 
 <?php get_footer(); ?>
